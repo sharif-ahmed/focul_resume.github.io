ad69a8bf16120e0ff6b10763fd1fe1c5af6abe13
sharif.ahmed <sharif@commlinkinfotech.com.bd>
2023-05-24 00:08:29 +0600

    swopper js and skillbar js jquery library added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,43 @@
 $(document).ready(function () {
+    //  Initialize Swiper
+    let swiper = new Swiper(".mySwiper", {
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
 
+    //Other Rest Libraries
+    $('.skillbar').skillBars({
+
+        // number start
+        from: 0,
+
+        // number end 
+        to: 100,
+
+        // animation speed
+        speed: 1000,
+
+        // how often the element should be up<a href="https://www.jqueryscript.net/time-clock/">date</a>d
+        interval: 100,
+
+        // the number of decimal places to show
+        decimals: 0,
+
+        // callback method for every time the element is updated,
+        onUpdate: null,
+
+        // callback method for when the element finishes updating
+        onComplete: null,
+
+        // CSS classes
+        classes: {
+            skillBarBar: '.skillbar-bar',
+            skillBarPercent: '.skill-bar-percent',
+        }
+
+    });
 });
+
+
+
